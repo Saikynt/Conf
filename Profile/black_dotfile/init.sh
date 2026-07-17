cbb2fbf0e14bbde32bec1bf7b197e7f1a12468aa
Saiky <122489575+Saikynt@users.noreply.github.com>
2026-07-16 17:52:59 -0700

    Create init.sh

---
+++ Profile/black_dotfile/init.sh
@@ -0,0 +1,8 @@
+pkill -9 vesktop
+vesktop &
+
+awww img ~/.config/wallpaper/wallpaper.png
+
+/home/saiky/.spicetify/spicetify config current_theme Spoty
+/home/saiky/.spicetify/spicetify apply
+dbus-launch spotify
